ac0bbf3261628b4e1bdd848a19838018b4f6b114
Fozzyishere <danielpham13092002@gmail.com>
2026-01-12 13:08:02 +0700

    fix: forgot to config this

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://asdf.dev',
+  site: 'https://fozzyishere.github.i.github.io',
+  base: '/portfolio',
   vite: {
     plugins: [tailwindcss()]
   }
